6f3c351355438c5aea95f3c779e6e000c7050d39
Lars Nolden <larsnolden@web.de>
2018-07-16 13:27:26 -0400

    added tooltip and axis labels

--- src/views/liquidity/components/Liquidity.jsx
+++ src/views/liquidity/components/Liquidity.jsx
@@ -5,6 +5,7 @@ import SelectCoinNum from 'elements/selectCoinNum/container/SelectCoinNum';
 import { Scatter } from 'react-chartjs-2';
 import * as zoom from 'chartjs-plugin-zoom';
 import Heading from 'elements/Heading';
+import formatNumber from 'formatNumber';
 
 const createConfig = data => ({
   labels: ['Scatter'],
@@ -25,6 +26,48 @@ const createConfig = data => ({
 });
 
 const chartOptions = {
+  scales: {
+    yAxes: [{
+      scaleLabel: {
+        display: true,
+        labelString: 'Volume last 24hrs (in Million)'
+      }
+    }],
+    xAxes: [{
+      scaleLabel: {
+        display: true,
+        labelString: 'Market Cap (in Billion)'
+      }
+    }],
+  },
+  tooltips: {
+    custom: function(tooltip) {
+      if (!tooltip) return;
+      // disable displaying the color box;
+      tooltip.displayColors = false;
+    },
+    callbacks: {
+      label: function(tooltipItem, data) {
+        // console.log(data.datasets[0].data[tooltipItem.index].tooltip[0].value)
+        return ''
+      },
+      title: function(tooltipItem, data) {
+        return data.datasets[0].data[tooltipItem[0].index].name;
+      },
+      afterLabel: function(tooltipItem, data) {
+        return data.datasets[0].data[tooltipItem.index].tooltipValues.map(pair => (
+          `${pair.label}: ${pair.value}`
+        ));
+      },
+    },
+    mode: 'nearest',
+    intersect: false,
+    yPadding: 10,
+    xPadding: 10,
+    caretSize: 6,
+    backgroundColor: '#ff9800',
+    borderWidth: 0,
+  },
   pan: {
     enabled: true,
     mode: 'xy'
@@ -33,12 +76,25 @@ const chartOptions = {
     enabled: true,
     mode: 'xy'
   },
-}
+};
 
 const mapCoinData = coins => coins.map(coin => ({
-  x: coin.marketCap,
-  y: coin.volume24,
+  x: coin.marketCap / 1000000000,
+  y: coin.volume24 / 1000000,
   radius: coin.percentChange24,
+  name: coin.name,
+  tooltipValues: [
+    {
+      label: 'Marketcap',
+      value: formatNumber(coin.marketCap)
+    },    {
+      label: 'Volume 24h',
+      value: formatNumber(coin.volume24)
+    },    {
+      label: 'Price change',
+      value: `${coin.percentChange24} %`
+    },
+  ]
 }));
 
 export default ({
